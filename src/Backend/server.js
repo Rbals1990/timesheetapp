bbc9ee03acad334475f1c70ac5fe88c82422c8b7
Rens Bals <rensbals1990@gmail.com>
2025-05-14 14:54:47 +0200

    edited files for backend production

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -14,12 +14,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, ".env") });
 
-/*
-//debugging
-console.log("Email User:", process.env.EMAIL_USER);
-console.log("Email Pass:", process.env.EMAIL_PASS ? "Loaded" : "Not loaded");
-console.log("Current directory:", __dirname);
-*/
 //routes
 
 const app = express();
@@ -27,7 +21,12 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors()); // Als je een frontend op een andere poort draait (bijv. React), moet je CORS inschakelen
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 // Pad naar de JSON-database
 const dbPath = path.join(__dirname, "users.json");
